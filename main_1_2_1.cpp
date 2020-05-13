5e12bc00c78127254419e87136b17537594c1744
Папа <papa@iMac-Ruslan.local>
2020-05-13 20:55:19 +0300

    1 2 1

--- main_1_2_1.cpp
+++ main_1_2_1.cpp
@@ -1,32 +1,55 @@
 #include <iostream>
 #include <stack>
-#define MAXN	100001
+#include <string>
+#include <string.h>
+#define MAXN	10001
 //Расстановка скобок в коде
 
 int main() {
-    char s[MAXN];
+    char s1[MAXN];
+	std::string s;
 	int i;
-	std::stack <char> brascets;
+	std::stack <std::pair <char, int>> brascets;
 	int ret;
+	std::pair<char, int> top;
 	
-	std::cin >> s;
+	std::getline(std::cin, s);
+//	ret = str.length();
+//		strcpy(s, str.c_str());
 	ret = 0;
 	i = -1;
 	while (s[++i])
 	{
 		if ('{' == s[i] || '[' == s[i] || '(' == s[i])
-			brascets.push(s[i]);
+			brascets.push(std::make_pair(s[i], i));
 		else if ('}' == s[i] || ']' == s[i] || ')' == s[i])
 		{
 			if (brascets.empty())
 			{
-				ret = 0;
+				std::cout << i + 1 << "\n";
+				return 0;
+			}
+			top = brascets.top();
+			brascets.pop();
+			if (('}' == s[i] && '{' != top.first) || (']' == s[i] && '[' != top.first) || (')' == s[i] && '(' != top.first))
+			{
+				ret = i;
 				break ;
 			}
 		}
 	}
-
-	
-	// put your code here
+	if (brascets.empty() && 0 == ret)
+		std::cout << "Success\n";
+	else if (ret)
+		std::cout << ret + 1 << "\n";
+	else 
+	{
+		while (!brascets.empty())
+		{
+			top = brascets.top();
+			brascets.pop();
+		}
+		std::cout << top.second + 1 << "\n";
+	}
     return 0;
 }
